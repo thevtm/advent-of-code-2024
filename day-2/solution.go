bdcc6139421f38fcfbaafe371e61b63a2cb9e4e1
Vinicius T. Manjabosco <978715+thevtm@users.noreply.github.com>
2024-12-04 11:57:46 +0100

    Day 2 Fix

--- day-2/solution.go
+++ day-2/solution.go
@@ -18,61 +18,55 @@ func absInt(x int) int {
 	return x
 }
 
-func check(direction string, a int, b int) (bool, string) {
+func validate(report []int, removed_index int) bool {
+	direction := "unknown"
 
-	diff := a - b
-	diff_abs := absInt(diff)
+	for i := 0; i < len(report)-1; i++ {
+		if i == removed_index {
+			continue
+		}
 
-	if diff_abs < 1 || diff_abs > 3 {
-		return false, direction
-	}
+		j := i + 1
 
-	if direction == "unknown" {
-		if diff > 0 {
-			direction = "increasing"
+		if j == removed_index {
+			j++
+		}
+
+		if j >= len(report) {
+			break
+		}
+
+		diff := report[i] - report[j]
+		diff_abs := absInt(diff)
+
+		if diff_abs < 1 || diff_abs > 3 {
+			return false
+		}
+
+		if direction == "unknown" {
+			if diff > 0 {
+				direction = "increasing"
+			} else {
+				direction = "decreasing"
+			}
 		} else {
-			direction = "decreasing"
-		}
-	} else {
-		if direction == "increasing" && diff < 0 {
-			return false, direction
-		} else if direction == "decreasing" && diff > 0 {
-			return false, direction
+			if direction == "increasing" && diff < 0 {
+				return false
+			} else if direction == "decreasing" && diff > 0 {
+				return false
+			}
 		}
 	}
 
-	return true, direction
-}
-
-func validLevelDiff(a int, b int) bool {
-	diff := a - b
-	diff_abs := absInt(diff)
-
-	return diff_abs >= 1 && diff_abs <= 3
-}
-
-func direction(a int, b int) string {
-	diff := a - b
-
-	if diff < 0 {
-		return "decreasing"
-	} else {
-		return "increasing"
-	}
-}
-
-func validate(report *[]int, removed int) {
-	for i := 0; i < len(*report)-1;i++ {
-		
-	}
+	return true
 }
 
 func main() {
 	// INPUTS
 
-	// input_path := "./input.txt"
+	input_path := "./input.txt"
 	// input_path := "./input-sample.txt"
-	input_path := "./input-sample-2.txt"
+	// input_path := "./input-sample-2.txt"
 
 	_, source_file_path, _, _ := runtime.Caller(0)
 	source_dir := filepath.Dir(source_file_path)
@@ -106,25 +100,15 @@ func main() {
 
 	safe_count := 0
 
-ReportsLoop:
 	for _, report := range reports {
-		direction := "unknown"
-
 		fmt.Println(report)
 
-		for i := 0; i < len(report)-1; i++ {
-			safe, new_direction := check(direction, report[i], report[i+1])
-
-			if !safe {
-				fmt.Println("Unsafe")
-				continue ReportsLoop
-			}
-
-			direction = new_direction
+		if validate(report, -1) {
+			fmt.Println("Safe")
+			safe_count++
+		} else {
+			fmt.Println("Unsafe")
 		}
-
-		fmt.Println("Safe")
-		safe_count++
 	}
 	fmt.Println()
 
@@ -135,47 +119,28 @@ ReportsLoop:
 
 	damped_safe_count := 0
 
-DampedReportsLoop:
+DampedReportLoop:
 	for _, report := range reports {
-		direction := "unknown"
-		dampener_used := false
-
 		fmt.Println(report)
 
-		for i := 0; i < len(report)-1; i++ {
-			safe, new_direction := check(direction, report[i], report[i+1])
+		if validate(report, -1) {
+			fmt.Println("Safe")
+			damped_safe_count++
+			continue
+		}
 
-			if !safe {
-				if dampener_used {
-					fmt.Println("Unsafe, i:", i)
-					continue DampedReportsLoop
-				}
-
-				if (i + 2) >= len(report) {
-					break
-				}
-
-				damped_safe, new_direction := check(direction, report[i], report[i+2])
-				direction = new_direction
-
-				if !damped_safe {
-					fmt.Println("Unsafe, i:", i)
-					continue DampedReportsLoop
-				} else {
-					fmt.Println("Dampener used, i:", i)
-					dampener_used = true
-					i++
-				}
-			} else {
-				direction = new_direction
+		for i := range len(report) {
+			if validate(report, i) {
+				fmt.Println("Safe without", report[i])
+				damped_safe_count++
+				continue DampedReportLoop
 			}
 		}
 
-		fmt.Println("Safe")
-		damped_safe_count++
+		fmt.Println("Unsafe")
 	}
 	fmt.Println()
 
-	fmt.Println("Problem 2 Result:", damped_safe_count) // 647
+	fmt.Println("Problem 2 Result:", damped_safe_count) // 658
 	fmt.Println()
 }
