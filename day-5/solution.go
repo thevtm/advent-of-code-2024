bf830304c9b8684280856f7687edbc95c8744a71
Vinicius T. Manjabosco <978715+thevtm@users.noreply.github.com>
2024-12-06 11:44:31 +0100

    Day 5 Problem 2

--- day-5/solution.go
+++ day-5/solution.go
@@ -17,7 +17,6 @@ func main() {
 
 	input_path := "./input.txt"
 	// input_path := "./input-sample.txt"
-	// input_path := "./input-sample-2.txt"
 
 	_, source_file_path, _, _ := runtime.Caller(0)
 	source_dir := filepath.Dir(source_file_path)
@@ -55,6 +54,7 @@ func main() {
 	// PROBLEM 1
 
 	sum_middle := 0
+	invalid_page_updates := make([][]string, 0)
 
 PageUpdatesLoop:
 	for _, page_update := range page_updates {
@@ -67,6 +67,7 @@ PageUpdatesLoop:
 				key := fmt.Sprintf("%s|%s", page, before_page)
 				if _, ok := before_map[key]; ok {
 					fmt.Println(" ", "invalid", key)
+					invalid_page_updates = append(invalid_page_updates, page_update)
 					continue PageUpdatesLoop
 				}
 			}
@@ -86,5 +87,31 @@ PageUpdatesLoop:
 
 	// PROBLEM 2
 
-	// fmt.Println("Problem 2 Result:", mas_count) // 1886
+	invalid_sum_middle := 0
+
+	for _, invalid_page_update := range invalid_page_updates {
+		fmt.Println(invalid_page_update)
+
+		slices.SortFunc(invalid_page_update, func(a string, b string) int {
+			if _, ok := before_map[fmt.Sprintf("%s|%s", a, b)]; ok {
+				return -1
+			}
+
+			if _, ok := before_map[fmt.Sprintf("%s|%s", b, a)]; ok {
+				return 1
+			}
+
+			return 0
+		})
+
+		fmt.Println(invalid_page_update)
+
+		middle_index := len(invalid_page_update) / 2
+		middle_page_int := lo.Must(strconv.Atoi(invalid_page_update[middle_index]))
+		fmt.Println(" ", "valid", "middle_page:", middle_page_int)
+		invalid_sum_middle += middle_page_int
+	}
+	fmt.Println()
+
+	fmt.Println("Problem 2 Result:", invalid_sum_middle) // 6336
 }
